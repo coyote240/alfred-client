386e47f65b3c59dfc79c5541829dc5a9e1cd56da
Adam A.G. Shamblin <adam.shamblin@gmail.com>
2018-03-11 15:03:11 -0600

    Hack in a basic data watcher and handler for alfred, turn on and off a light

--- alfred_client/application.py
+++ alfred_client/application.py
@@ -8,9 +8,11 @@ from tornado.iostream import IOStream
 from tornado.web import URLSpec
 from tornado.httpserver import HTTPServer
 from tornado.options import define, options
-from alfred_client.message import PushData
+from alfred_client.message import Request, PushData
+from alfred_client.packet import struct
 from alfred_client import handlers
 
+import RPi.GPIO as GPIO
 
 define('socket_address', type=str, default='/var/run/alfred.sock',
        help='Path to the alfred socket')
@@ -22,6 +24,9 @@ define('bat_interface', type=str, default='bat0',
        help='The interface on which alfred listens')
 define('web_interface', type=str, default='eth0',
        help='The interface to which the web application will bind')
+define('ledpin', type=int, default=11,
+       help='Pin used by demo')
+
 define('config', help='Path to config file',
        callback=lambda path: options.parse_config_file(path, final=False))
 
@@ -33,6 +38,7 @@ class Application(tornado.web.Application):
         self.init_signal_handlers()
         settings = self.init_settings()
         self.init_interfaces()
+        self.init_gpio()
 
         super().__init__(self.handlers, **settings)
 
@@ -75,8 +81,13 @@ class Application(tornado.web.Application):
             signal.signal(signal.SIGQUIT, self.interrupt_handler)
 
     def interrupt_handler(self, signum, frame):
+        GPIO.cleanup()
         tornado.ioloop.IOLoop.instance().add_callback_from_signal(
                 lambda: tornado.ioloop.IOLoop.instance().stop())
+
+    def init_gpio(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(options.ledpin, GPIO.OUT)
 
     def host_info_task(self):
         host_info = PushData()
@@ -89,6 +100,35 @@ class Application(tornado.web.Application):
         with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
             s.connect(options.socket_address)
             s.sendall(bytes(host_info))
+
+    def alfred_task_reader(self):
+        request = Request()
+        request.requested_type = 67
+        request.transaction_id = 1
+
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+            s.connect(options.socket_address)
+            s.sendall(bytes(request))
+
+            retval = []
+            while True:
+                header = s.recv(struct.alfred_tlv.sizeof())
+                if len(header) == 0:
+                    break
+
+                tlv = struct.alfred_tlv.parse(header)
+                body = s.recv(tlv.length)
+                data_block = struct.alfred_push_data.parse(body)
+                retval.append(data_block.alfred_data[0])
+
+        # retval should be interpreted and put on a queue, but we'll hack it
+        # for now
+        for r in retval:
+            val = r.data.decode()
+            if val == 'on':
+                GPIO.output(options.ledpin, GPIO.HIGH)
+            else:
+                GPIO.output(options.ledpin, GPIO.LOW)
 
     def discovery_address(self):
         message = json.dumps({
@@ -112,6 +152,8 @@ class Application(tornado.web.Application):
         # This should really point to a task queue
         tornado.ioloop.PeriodicCallback(self.discovery_address, 5000).start()
         tornado.ioloop.PeriodicCallback(self.host_info_task, 5000).start()
+        tornado.ioloop.PeriodicCallback(self.alfred_task_reader, 500).start()
+
         tornado.ioloop.IOLoop.current().start()
 
 
